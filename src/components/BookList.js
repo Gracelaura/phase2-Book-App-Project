b5ecc6431429ec40657f506872db0b33fcc77b7e
Gracelaura <gracenjuguna05@gmail.com>
2022-10-06 10:33:53 +0300

    Creates a db.json file, added a fetch, some styling

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "../App"
 
 function BookList() {
-    return<div>BookList</div>
+    const[books, setBooks] = useState([])
+
+    function fetchData() {
+        fetch("http://localhost:8000/books")
+        .then(resp=>resp.json())
+        .then(result=>setBooks(result))
+    }
+
+    useEffect(() => {
+       fetchData() 
+    }, [])
+
+
+    return <div className="book-list">
+        {books.map((book) =>(
+            <div key={book.id}>
+                <div>
+                    <h2>{book.title}</h2>
+                </div>
+                <div>
+                    <img src={book.image_url} alt="#" />
+                </div>
+                <div>
+                    <button>Add to Favorites</button>
+                </div>
+            </div>
+        ))}
+    </div>
+
+        
+    
         
     
 }
