f16429581fe6cfe445091209d82131af4b2f7d3d
Gracelaura <gracenjuguna05@gmail.com>
2022-10-07 13:41:48 +0300

    write a function to add a book to favorites as well as remove it from favorites

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,9 +1,40 @@
 import React from "react";
 import "../App";
+import {useAppContext} from "./Context/appContext"
 
 
 function Favorites() {
-    return <div className="favorites">Favorites</div>
+    const {favorites, addToFavorites, removeFromFavorites} = useAppContext()
+
+    function favoritesChecker(id) {
+      const boolean = favorites.some((book) => book.id === id)
+      return boolean  
+    }
+
+    return <div className="favorites">
+        {favorites.length > 0 ? favorites.map((book) => (
+            <div key={book.id} className="book" >
+                <div>
+                    <h4>{book.title}</h4>
+                </div>
+                <div>
+                    <img src={book.image_url} alt="#" />
+                </div>
+                <div>
+                    {favoritesChecker(book.id) ? (
+                        <button onClick={() => removeFromFavorites(book.id)}>
+                            Remove from Favorites
+                        </button>
+                    ) : (
+                        <button onClick={() => addToFavorites(book)}>
+                            Add to Favorites
+                        </button>
+                    )}
+
+                </div>
+            </div>
+        )):<h1>You don't have any favorite books yet</h1>}
+    </div>
 }
 
 export default Favorites
