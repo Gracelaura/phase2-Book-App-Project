039975a0a41a0659df73b028ce8404b8f7127f33
Gracelaura <gracenjuguna05@gmail.com>
2022-10-09 00:20:04 +0300

    add  useState hook, delete Booklist component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useState} from 'react';
 import {Routes, Route} from "react-router-dom"
 import BookList from './components/BookList';
 import BookDetails from './components/BookDetails';
@@ -7,13 +8,20 @@ import Footer from './components/Footer';
 import Favorites from './components/Favorites';
 
 function App() {
+  const [myFav, setMyFav] = useState([])
+
+  function addBookToFav(book){
+console.log(book);
+myFav.includes(book)?setMyFav(myFav):setMyFav(myFav=>[...myFav,book])
+  }
   return (
     <div className="App">
       <Navbar />
+      {/* <BookList />  */}
       <Routes>
-        <Route path="/" element={<BookList />} />
+        <Route path="/" element={<BookList addBookToFav={addBookToFav}/>} />
         <Route path="/book/:id" element={<BookDetails />} />
-        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/favorites" element={<Favorites myFav={myFav} />} />
       </Routes>
       <Footer />
     </div>
