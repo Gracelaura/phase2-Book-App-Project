b255cb525eb2a0e44c87edabe1b35032fe3c05b3
Gracelaura <gracenjuguna05@gmail.com>
2022-10-09 00:13:18 +0300

    comment out code

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,40 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App";
-import {useAppContext} from "./Context/appContext"
+// import { useAppContext } from "./Context/appContext"
 
 
-function Favorites() {
-    const {favorites, addToFavorites, removeFromFavorites} = useAppContext()
+function Favorites({ myFav }) {
+    // const { favorites, addToFavorites, removeFromFavorites } = useAppContext()
 
-    function favoritesChecker(id) {
-      const boolean = favorites.some((book) => book.id === id)
-      return boolean  
-    }
+    // function favoritesChecker(id) {
+    //     const boolean = favorites.some((book) => book.id === id)
+    //     return boolean
+    // }
 
-    return <div className="favorites">
-        {favorites.length > 0 ? favorites.map((book) => (
-            <div key={book.id} className="book" >
-                <div>
-                    <h4>{book.title}</h4>
-                </div>
-                <div>
-                    <img src={book.image_url} alt="#" />
-                </div>
-                <div>
-                    {favoritesChecker(book.id) ? (
-                        <button onClick={() => removeFromFavorites(book.id)}>
-                            Remove from Favorites
-                        </button>
-                    ) : (
-                        <button onClick={() => addToFavorites(book)}>
-                            Add to Favorites
-                        </button>
-                    )}
-
-                </div>
+    return (
+        <>
+            <div className="favorites">
+                {/* {favorites.length > 0 ? (
+            <h1>You have favorite books</h1>
+        ) :(
+         <h1>You don't have any favorite books yet</h1>
+        )} */}
+        {
+            myFav.map((fav,ind)=>{
+                return (
+                    <div key={ind}>
+                        <img src={fav.image_url} alt='book image'/>
+                        <h3>{fav.title}</h3>
+                        <p>By:<b>{fav.authors}</b></p>
+                    </div>
+                )
+            })
+        }
             </div>
-        )):<h1>You don't have any favorite books yet</h1>}
-    </div>
+        </>
+
+
+    )
 }
 
 export default Favorites
