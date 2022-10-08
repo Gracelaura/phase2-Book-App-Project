522a7467172dd757b5c054c395deda697ad6cfc5
Gracelaura <gracenjuguna05@gmail.com>
2022-10-09 00:16:25 +0300

    add useNavigate hook, add fetch in a useEffect hook

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,39 +1,47 @@
 import React, { useState, useEffect } from "react";
 import "../App"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 
 function BookDetails() {
     const [book, setBook] = useState({})
     const { id } = useParams()
+    const navigate = useNavigate()
 
-    function fetchData() {
+
+
+    useEffect(() => {
         fetch(`http://localhost:8000/books/${id}`)
             .then(resp => resp.json())
             .then(result => setBook(result))
+    }, [id])
+    // console.log(res);
+
+    function handleBtnClick() {
+        navigate(-1)
     }
+    return (
+        <>
+            <div className="back-btn">
+                <button className="btn" onClick={handleBtnClick}>BACK</button>
+            </div>
+            <div className="book-details">
 
-    useEffect(() => {
-        fetchData()
-    },)
-
-
-
-    return(
-    <div className="book-details">
-        <div className="book-image">
-            <h2>{book?.title}</h2>
-            <img src={book?.image_url} alt="#" />
-        </div>
-        <div className="book-description">
-            <h2>Description</h2>
-            <p>{book?.description}</p>
-            <h2>Authors</h2>
-            <p>{book?.authors}</p>
-            <h2>Genres</h2>
-            <p>{book?.genres}</p>
-        </div>
-    </div>)
+                <div className="book-image">
+                    <h2>{book.title}</h2>
+                    <img src={book?.image_url} alt="#" />
+                </div>
+                <div className="book-description">
+                    <h2>Description</h2>
+                    <p>{book?.description}</p>
+                    <h2>Authors</h2>
+                    <p>{book?.authors}</p>
+                    <h2>Genres</h2>
+                    <p>{book?.genres}</p>
+                </div>
+            </div>
+        </>
+    )
 
 
 
