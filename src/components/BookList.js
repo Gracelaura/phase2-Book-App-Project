9eccd228f71efb4df6bcb45b72b1bf45571a99b8
Gracelaura <gracenjuguna05@gmail.com>
2022-10-09 00:13:50 +0300

    delete useAppContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,57 +1,62 @@
 import React, { useState, useEffect } from "react";
 import "../App"
-import { useAppContext } from "./Context/appContext"
+// import { useAppContext } from "./Context/appContext"
 import {useNavigate} from "react-router-dom"
 
 
 
-function BookList() {
+function BookList({addBookToFav}) {
     const [books, setBooks] = useState([])
-
-    const { favorites, addToFavorites, removeFromFavorites } = useAppContext()
-
     const navigate = useNavigate()
+    const [isClicked, setIsClicked] = useState(false)
 
-    function favoritesChecker(id) {
-        const boolean = favorites.some((book) => book.id === id)
-        return boolean
-    }
+    // const { favorites, addToFavorites, removeFromFavorites } = useAppContext()
 
-    function fetchData() {
+    // const navigate = useNavigate()
+
+    // function favoritesChecker(id) {
+    //     const boolean = favorites.some((book) => book.id === id)
+    //     return boolean
+    // }
+
+const  handleBookDisplay = (book)=>{
+    navigate(`/book/${book.id}`)
+}
+
+function handleMyFavourites (book){
+    addBookToFav(book);
+    setIsClicked(!isClicked)
+
+}
+    useEffect(() => {
         fetch("http://localhost:8000/books")
             .then(resp => resp.json())
             .then(result => setBooks(result))
-    }
-
-    useEffect(() => {
-        fetchData()
     }, [])
 
+    return (
+        <>
+            <div className="book-list">
+                {books.map((book) => {
+                    return (
+                        <div key={book.id} className="book" >
+                            <h4>{book.title}</h4>
+                            <div>
+                                <img src={book.image_url} alt="#" onClick={()=>{
+                            handleBookDisplay(book)
+                        }} />
+                            </div>
+                            <div>
+                                <button onClick={()=>handleMyFavourites(book)}>{isClicked?'remove from favorites':'Add to Favorites'}</button>
+                            </div>
+                        </div>
+                    )
+                })}
 
-    return <div className="book-list">
-        {books.map((book) => (
-            <div key={book.id} className="book" >
-                <div>
-                    <h4>{book.title}</h4>
-                </div>
-                <div>
-                    <img src={book.image_url} alt="#" onClick={()=>navigate(`/book/${book.id}`)}/>
-                </div>
-                <div>
-                    {favoritesChecker(book.id) ? (
-                        <button onClick={() => removeFromFavorites(book.id)}>
-                            Remove from Favorites
-                        </button>
-                    ) : (
-                        <button onClick={() => addToFavorites(book)}>
-                            Add to Favorites
-                        </button>
-                    )}
-
-                </div>
             </div>
-        ))}
-    </div>
+        </>
+    )
+
 }
 
 export default BookList
